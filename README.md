84f3d3dba070ae69d2a0f0da9e408bcb21c09c21
Sean Seefried <sean.seefried@gmail.com>
2019-05-05 21:30:00 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+Symlink into all the sub-repos that you want to deploy.
+Each one of these must have a `deploy.sh` script
---
+++ deploy-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+[ "$SEANSEEFRIED_DOT_ORG_MACHINE" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+[ "$SEANSEEFRIED_DOT_ORG_PATH" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+
+tilthydeath-nixos/nixos-switch.sh
+
+echo
+echo
+echo "=== Deploy index.html =========================================="
+scp index.html "$SEANSEEFRIED_DOT_ORG_MACHINE:$SEANSEEFRIED_DOT_ORG_PATH"
+
+for i in $(find -L . -name 'deploy.sh'); do
+  if [ -x $i ]; then
+    echo
+    echo
+    echo "=== Running $i ============================================"
+    ./$i
+  fi
+done
