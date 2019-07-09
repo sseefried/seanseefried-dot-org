15c32a9167b1a915f2b61dabaeaf07430b1a3ddc
Sean Seefried <sean.seefried@gmail.com>
2019-07-09 11:21:29 +1000

    Update deploy scripts

--- deploy-all.sh
+++ deploy-all.sh
@@ -3,8 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-[ "$SEANSEEFRIED_DOT_ORG_MACHINE" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
-[ "$SEANSEEFRIED_DOT_ORG_PATH" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG SSH path to remote directory"; exit 1; }
+[ "$SEANSEEFRIED_DOT_ORG_MACHINE" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG_MACHINE SSH path to remote directory"; exit 1; }
+[ "$SEANSEEFRIED_DOT_ORG_PATH" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG_PATH SSH path to remote directory"; exit 1; }
 
 tilthydeath-nixos/nixos-switch.sh
 
