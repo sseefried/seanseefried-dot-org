38851630d99f7378ae666ac12c8044636252a70f
Sean Seefried <sean.seefried@gmail.com>
2019-07-09 11:39:47 +1000

    Add goal-pride and change deploy scripts

--- deploy-root.sh
+++ deploy-root.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 [ "$SEANSEEFRIED_DOT_ORG_MACHINE" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG_MACHINE SSH path to remote directory"; exit 1; }
 [ "$SEANSEEFRIED_DOT_ORG_PATH" != "" ] || { echo "Set SEANSEEFRIED_DOT_ORG_PATH SSH path to remote directory"; exit 1; }
 
-tilthydeath-nixos/nixos-switch.sh
+echo
+echo
+echo "=== Deploy root files =========================================="
+cd $DIR/root-files
 
-./deploy-root.sh
-
-for i in $(find -L . -name 'deploy.sh'); do
-  if [ -x $i ]; then
-    echo
-    echo
-    echo "=== Running $i ============================================"
-    ./$i
-  fi
+for i in *.html; do
+  scp $i "$SEANSEEFRIED_DOT_ORG_MACHINE:$SEANSEEFRIED_DOT_ORG_PATH"
 done
